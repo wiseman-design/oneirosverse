de352c3b9ec590ba2446361639a6a617dee89693
wiseman-design <spyros20041@gmail.com>
2026-03-12 09:42:39 +0200

    bind to 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -52,6 +52,7 @@ app.delete("/api/projects/:id", async (req, res) => {
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
